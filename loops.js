434335d03384575ff89ccd94db942d43b21b1f16
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-08 05:29:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -5,7 +5,8 @@ function forLoop(array) {
     }
     
     else {
-      array[i] = "I am 1 strange loop.";
+      var x = i;
+      array[x] = "I am ${i} strange loop.";
     }
   }
   return array;
