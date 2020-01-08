81d665b0f2f63dffa63e0571855265139ad0821f
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-08 05:30:23 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,12 +1,11 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (i == 1) {
+    if (i === 1) {
       array[i] = "I am 1 strange loop.";
     }
     
     else {
-      var x = i;
-      array[i] = "I am ${x} strange loop.";
+      array[i] = "I am ${i} strange loop.";
     }
   }
   return array;
