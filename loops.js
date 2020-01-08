460e1727559f4853b7e711e6aded309de292d9a9
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-08 05:43:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,13 +19,14 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(num) {
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
+  var i = 0;
+    function incrementVariable() {
+      i = i + 1;
+      return i;
+    }
  
-do {
-  console.log("I run once regardless");
-} while (incrementVariable() < 5);
+  do {
+    console.log("I run once regardless");
+  } while (incrementVariable() < 5);
 }
 
